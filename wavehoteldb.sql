feb4f1eacff69120c18aaae22f88d7fd42bed43e
AnPhamHong <an.ph410@gmail.com>
2020-11-16 00:40:18 +0700

    Them database va dac ta use case he thong

---
+++ wavehoteldb.sql
@@ -0,0 +1,117 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: wavehoteldb
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `cthoadon`
+--
+
+LOCK TABLES `cthoadon` WRITE;
+/*!40000 ALTER TABLE `cthoadon` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cthoadon` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `hoadon`
+--
+
+LOCK TABLES `hoadon` WRITE;
+/*!40000 ALTER TABLE `hoadon` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hoadon` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `khachhang`
+--
+
+LOCK TABLES `khachhang` WRITE;
+/*!40000 ALTER TABLE `khachhang` DISABLE KEYS */;
+/*!40000 ALTER TABLE `khachhang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `loaikhach`
+--
+
+LOCK TABLES `loaikhach` WRITE;
+/*!40000 ALTER TABLE `loaikhach` DISABLE KEYS */;
+/*!40000 ALTER TABLE `loaikhach` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `loaiphong`
+--
+
+LOCK TABLES `loaiphong` WRITE;
+/*!40000 ALTER TABLE `loaiphong` DISABLE KEYS */;
+/*!40000 ALTER TABLE `loaiphong` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `nhanvien`
+--
+
+LOCK TABLES `nhanvien` WRITE;
+/*!40000 ALTER TABLE `nhanvien` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nhanvien` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `phieuthuephong`
+--
+
+LOCK TABLES `phieuthuephong` WRITE;
+/*!40000 ALTER TABLE `phieuthuephong` DISABLE KEYS */;
+/*!40000 ALTER TABLE `phieuthuephong` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `phong`
+--
+
+LOCK TABLES `phong` WRITE;
+/*!40000 ALTER TABLE `phong` DISABLE KEYS */;
+/*!40000 ALTER TABLE `phong` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `quydinh`
+--
+
+LOCK TABLES `quydinh` WRITE;
+/*!40000 ALTER TABLE `quydinh` DISABLE KEYS */;
+/*!40000 ALTER TABLE `quydinh` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-16  0:34:16
